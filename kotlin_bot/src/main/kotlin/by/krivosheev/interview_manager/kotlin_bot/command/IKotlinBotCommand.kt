37116a2e8150bd3b6bffc1af1f083c7e0031e0d3
Egor Krivosheev <egorkaxddd@gmail.com>
2025-03-30 22:59:40 +0300

    IMB-13: добавлен Kotlin бот

---
+++ kotlin_bot/src/main/kotlin/by/krivosheev/interview_manager/kotlin_bot/command/IKotlinBotCommand.kt
@@ -0,0 +1,8 @@
+package by.krivosheev.interview_manager.kotlin_bot.command
+
+import org.telegram.telegrambots.extensions.bots.commandbot.commands.IBotCommand
+
+/**
+ * Интерфейс для сбора всех команд "Kotlin" бота.
+ */
+interface IKotlinBotCommand : IBotCommand
